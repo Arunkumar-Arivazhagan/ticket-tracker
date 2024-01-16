bb6b48d5cb014b604c42e77dc3aa1fd2fafed6a6
Arunkumar <arunkumar041093@gmail.com>
2024-01-17 12:17:37 +1300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Employee } from "./components/Employee";
 import employees from "./data/employees.ts";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-center mb-4 mt-4">Tracker</h1>
-      <div className="flex flex-wrap gap-8">
-        {employees.map((employee) => {
-          return <Employee name={employee.name} role={employee.role} />;
-        })}
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <h1 className="text-3xl text-center mb-4 mt-4">Tracker</h1>
+    <div className="grid grid-cols-3 gap-8">
+      {employees.map(({ id, name, role }) => (
+        <Employee key={id} name={name} role={role} />
+      ))}
+    </div>
+  </>
+);
 
 export default App;
--- src/components/Employee.tsx
+++ src/components/Employee.tsx
@@ -8,23 +8,20 @@ type EmployeeProps = {
 export const Employee = ({ name, role }: EmployeeProps) => {
   const [counter, setCounter] = useState(0);
 
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  };
+  const incrementCounter = () => setCounter(counter + 1);
 
   const decrementCounter = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
+    if (counter > 0) setCounter(counter - 1);
   };
 
   return (
-    <div className="border-green-500 border-2 w-64 mb-8 text-center">
+    <div className="border-green-500 border-2 mb-8 text-center">
       <p>Name: {name}</p>
       <p>Role: {role}</p>
       <div className="border-green-500 border-2 p-2 w-32 m-auto mb-8 mt-4 text-center">
         <p>Counter: </p>
         <p>{counter}</p>
+        {counter >= 10 && <p>Good Job!</p>}
         <button className="mr-6" onClick={decrementCounter}>
           -
         </button>
