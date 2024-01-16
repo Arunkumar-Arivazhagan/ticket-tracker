33bd0a8ea115da9e7345a6d7bf05e1b3b99b9655
Arunkumar <arunkumar041093@gmail.com>
2024-01-17 11:07:48 +1300

    counter is working

--- src/components/Employee.tsx
+++ src/components/Employee.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 type EmployeeProps = {
   name: string;
   role: string;
 };
 
-export const Employee = (props: EmployeeProps) => {
+export const Employee = ({ name, role }: EmployeeProps) => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="border-green-500 border-2 w-64 mb-8 text-center">
-      <p>Employee Name: {props.name}</p>
-      <p>Role: {props.role}</p>
+      <p>Employee Name: {name}</p>
+      <p>Role: {role}</p>
+      <p>Counter: {counter}</p>
+      <button onClick={incrementCounter}>+</button>
+      <br></br>
+      <button onClick={decrementCounter}>-</button>
     </div>
   );
 };
