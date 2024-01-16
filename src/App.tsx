ff9da1218956fc2c7c99c59a95ed722679d51bf5
Arunkumar <arunkumar041093@gmail.com>
2024-01-17 12:31:26 +1300

    mobile & tab comp

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import employees from "./data/employees.ts";
 const App = () => (
   <>
     <h1 className="text-3xl text-center mb-4 mt-4">Tracker</h1>
-    <div className="grid grid-cols-3 gap-8">
+    <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4">
       {employees.map(({ id, name, role }) => (
         <Employee key={id} name={name} role={role} />
       ))}
