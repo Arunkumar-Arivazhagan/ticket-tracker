7cda0ff3e565385f2c5a4b1b22ec48249c73bb9e
Arunkumar <arunkumar041093@gmail.com>
2024-01-23 10:24:04 +1300

    final commit - session 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
-import TicketTracker from "./components/TicketTracker.tsx";
+import { useState, Fragment, useEffect } from "react";
 
-const App = () => <TicketTracker />;
+type PokemonResponse = {
+  results: Pokemon[];
+};
+type Pokemon = {
+  name: string;
+  url: string;
+};
+const App = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  const fetchPokemon = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const data: PokemonResponse = await response.json();
+    setPokemons(data.results);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      <h1>Pokemon</h1>
+      {pokemons.map(({ name }, index) => {
+        const id = index + 1;
+        const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+        return (
+          <Fragment key={name}>
+            <p>{name}</p>
+            <img src={src} alt={name} />
+          </Fragment>
+        );
+      })}
+      {/* <button onClick={fetchPokemon}>Show Pokemon</button> */}
+    </>
+  );
+};
 
 export default App;
